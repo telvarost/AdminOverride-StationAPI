e603cb322336e69490091a3fd8f386a8f6d90705
telvarost <telontelvarost@gmail.com>
2024-09-14 14:43:08 -0400

    Add keybindings and update biny mappings version

---
+++ src/main/java/com/github/telvarost/adminoverride/events/KeyPressedListener.java
@@ -0,0 +1,31 @@
+package com.github.telvarost.adminoverride.events;
+
+import com.github.telvarost.adminoverride.ModHelper;
+import com.github.telvarost.adminoverride.events.init.KeyBindingListener;
+import net.fabricmc.loader.api.FabricLoader;
+import net.mine_diver.unsafeevents.listener.EventListener;
+import net.minecraft.client.Minecraft;
+import net.modificationstation.stationapi.api.client.event.keyboard.KeyStateChangedEvent;
+import org.lwjgl.input.Keyboard;
+
+public class KeyPressedListener {
+    Minecraft minecraft = null;
+
+    @EventListener
+    public void keyPress(KeyStateChangedEvent event) {
+        if (minecraft == null) {
+            minecraft = ((Minecraft) FabricLoader.getInstance().getGameInstance());
+        }
+
+        if (Keyboard.getEventKeyState() && minecraft.currentScreen == null) {
+            // Hotbar Slots
+            if (minecraft.currentScreen == null) {
+                if (Keyboard.isKeyDown(KeyBindingListener.toggleSprint.code)) {
+                    minecraft.player.inventory.selectedSlot = 0;
+                } else if (Keyboard.isKeyDown(KeyBindingListener.toggleFlight.code)) {
+                    minecraft.player.inventory.selectedSlot = 1;
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/github/telvarost/adminoverride/events/init/KeyBindingListener.java
@@ -0,0 +1,18 @@
+package com.github.telvarost.adminoverride.events.init;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.mine_diver.unsafeevents.listener.EventListener;
+import net.minecraft.client.option.KeyBinding;
+import net.modificationstation.stationapi.api.client.event.option.KeyBindingRegisterEvent;
+import org.lwjgl.input.Keyboard;
+
+public class KeyBindingListener {
+    public static KeyBinding toggleFlight;
+    public static KeyBinding toggleSprint;
+
+    @EventListener
+    public void registerKeyBindings(KeyBindingRegisterEvent event) {
+        event.keyBindings.add(toggleFlight = new KeyBinding("Fly", Keyboard.KEY_R));
+        event.keyBindings.add(toggleSprint = new KeyBinding("Sprint", Keyboard.KEY_LCONTROL));
+    }
+}
